9d58fc01a9225ae3c187c353875af554c123ae5a
Jeremy PASTOURET <pastouret.jeremy@gmail.com>
2023-11-08 10:38:46 +0100

    reformat amazon

--- src/amazon.js
+++ src/amazon.js
@@ -63,122 +63,122 @@ function onClickBuy(event){
     }
 }
 
-
-// console.log("jdkfdjfjdkfjdkfjdkf")
-document.body.style.border = "5px solid red";
-
-const toto = document.getElementById("corePrice_feature_div");
-console.log(toto)
-/*const iframe = document.createElement('script');
-
-iframe.setAttribute('src','https://impactco2.fr/iframe.js');
-iframe.setAttribute("data-type", "numerique");
-iframe.setAttribute("data-search", "?theme=night");
+function showBanniere(){
+    // Make a banner
+    const bannerAmazon = document.getElementById("nav-belt")
+    const bannerKickStarter = document.createElement("div");
 
 
-console.log(iframe)*/
+    // const mode = 'CA_ECHO'; //CA_ECHO || Fusee
+    bannerKickStarter.style.height = '33px';
 
-const pItem = document.createElement("p")
-pItem.innerText = "La black friday c'est nuls"
+    // if('CA_ECHO' === mode) {
 
-if(toto){
-    toto.appendChild(pItem)
-}
+        let messages = [
+            "Notre mission, vous faire acheter toujours plus ! + 16 % de croissance annuelle grâce à vous ❤️",
+            "Un peu d'argent sur votre compte ? On vous aide à le dépenser rapidement !",
+            "Besoin d'un objet inutile ? On a la solution pour vous !",
+            "On teste de nouveaux robots humanoïdes pour préparer vos commandes => - d'employés, + 🤑 ",
+            "Des livraisons quasiment gratuites pour vous. Les gaz à effet de serre sont offerts"
+        ]
 
-
-// Make a banner
-const bannerAmazon = document.getElementById("nav-belt")
-const bannerKickStarter = document.createElement("div");
-
-
-const mode = 'CA_ECHO'; //CA_ECHO || Fusee
-bannerKickStarter.style.height = '33px';
-
-if('CA_ECHO' === mode) {
-
-    let messages = [
-        "Notre mission, vous faire acheter toujours plus ! + 16 % de croissance annuelle grâce à vous ❤️",
-        "Un peu d'argent sur votre compte ? On vous aide à le dépenser rapidement !",
-        "Besoin d'un objet inutile ? On a la solution pour vous !",
-        "On teste de nouveaux robots humanoïdes pour préparer vos commandes => - d'employés, + 🤑 ",
-        "Des livraisons quasiment gratuites pour vous. Les gaz à effet de serre sont offerts"
-    ]
-
-    let positionMessage = Math.floor(Math.random() * (messages.length - 0 + 1) + 0);
+        let positionMessage = Math.floor(Math.random() * (messages.length - 0 + 1) + 0);
 
 //source : https://www.lesechos.fr/industrie-services/conso-distribution/amazon-a-depasse-les-10-milliards-deuros-de-chiffre-daffaires-en-france-1981779
 //     bannerKickStarter.textContent = "+ de 10 000 000 000€ de chiffre d'affaires grâce à vos achats ! Merci pour votre confiance ❤️";
-    bannerKickStarter.textContent = messages[positionMessage];
+        bannerKickStarter.textContent = messages[positionMessage];
 
 // bannerKickStarter.style.background = '#fafafa';
-    bannerKickStarter.style.textAlign = 'center';
-    bannerKickStarter.style.color = '#fafafa';
-    bannerKickStarter.classList.add("a-size-large");
-    bannerKickStarter.classList.add("product-title-word-break");
-}
-else if('FUSEE' === mode){
-    let progressVal = '10%';
-    let progress = document.createElement("div");
-    progress.classList.add("ldtimercont");
-    progress.style.width= '70%';
-    progress.style.margin = 'auto';
+        bannerKickStarter.style.textAlign = 'center';
+        bannerKickStarter.style.color = '#fafafa';
+        bannerKickStarter.classList.add("a-size-large");
+        bannerKickStarter.classList.add("product-title-word-break");
+    // }
+// else if('FUSEE' === mode){
+//     let progressVal = '10%';
+//     let progress = document.createElement("div");
+//     progress.classList.add("ldtimercont");
+//     progress.style.width= '70%';
+//     progress.style.margin = 'auto';
+//
+//     let state = document.createElement("div");
+//     state.classList.add('statebar');
+//     state.style.height='25px';
+//
+//     progress.appendChild(state);
+//
+//
+//     let prog = document.createElement("div");
+//     prog.classList.add('progbar')
+//     prog.style.width = progressVal;
+//     prog.style.height='25px';
+//     prog.style.backgroundColor = '#3480BF';
+//
+//
+//     progress.appendChild(prog);
+//
+//     let progbartick = document.createElement("div");
+//     progbartick.classList.add('progbartick')
+//     progbartick.style.width = progressVal;
+//     progbartick.style.height='29px';
+//
+//     progress.appendChild(progbartick);
+//
+//     bannerKickStarter.appendChild(progress);
+// }
 
-    let state = document.createElement("div");
-    state.classList.add('statebar');
-    state.style.height='25px';
-
-    progress.appendChild(state);
-
-
-    let prog = document.createElement("div");
-    prog.classList.add('progbar')
-    prog.style.width = progressVal;
-    prog.style.height='25px';
-    prog.style.backgroundColor = '#3480BF';
-
-
-    progress.appendChild(prog);
-
-    let progbartick = document.createElement("div");
-    progbartick.classList.add('progbartick')
-    progbartick.style.width = progressVal;
-    progbartick.style.height='29px';
-
-    progress.appendChild(progbartick);
-
-    bannerKickStarter.appendChild(progress);
-}
-
-bannerAmazon.parentNode.insertBefore(bannerKickStarter, bannerAmazon.nextSibling);
-
-
-
-//description de produit
-let descriptionProduit = document.getElementById("productDescription_feature_div")
-if(!descriptionProduit)
-    descriptionProduit = document.getElementById('device-dp-recommendations_feature_div')
-
-if(descriptionProduit){
-    let divMessage = document.createElement("div");
-    let imgDechet = document.createElement("img");
-    let pText = document.createElement("h2");
-    pText.classList.add('default');
-    imgDechet.src = "https://unsplash.com/photos/7_TSzqJms4w/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8NHx8ZGVjaGV0fGZyfDB8fHx8MTY5OTEzNDEwMHww&force=true&w=640"
-
-
-    pText.innerText = 'Contribuez à notre montagne de déchets non-recyclés en achetant nos produits 🤑';
-    divMessage.appendChild(pText);
-
-    imgDechet.style.margin = "auto"
-
-    divMessage.appendChild(imgDechet);
-
-    descriptionProduit.parentNode.insertBefore(divMessage, descriptionProduit.nextSibling);
+    bannerAmazon.parentNode.insertBefore(bannerKickStarter, bannerAmazon.nextSibling);
 }
 
-//Le saviez-vous panier
-const afterPricePanier = document.getElementById("deselect-all")
-if(afterPricePanier) {
+function displayMessagePrix(){
+    // console.log("jdkfdjfjdkfjdkfjdkf")
+// document.body.style.border = "5px solid red";
+//
+// const toto = document.getElementById("corePrice_feature_div");
+// console.log(toto)
+    /*const iframe = document.createElement('script');
+
+    iframe.setAttribute('src','https://impactco2.fr/iframe.js');
+    iframe.setAttribute("data-type", "numerique");
+    iframe.setAttribute("data-search", "?theme=night");
+
+
+    console.log(iframe)*/
+
+// const pItem = document.createElement("p")
+// pItem.innerText = "La black friday c'est nuls"
+//
+// if(toto){
+//     toto.appendChild(pItem)
+// }
+}
+
+function displayDescription(){
+    //description de produit
+    let descriptionProduit = document.getElementById("productDescription_feature_div")
+    if(!descriptionProduit)
+        descriptionProduit = document.getElementById('device-dp-recommendations_feature_div')
+
+    if(descriptionProduit){
+        let divMessage = document.createElement("div");
+        let imgDechet = document.createElement("img");
+        let pText = document.createElement("h2");
+        pText.classList.add('default');
+        imgDechet.src = "https://unsplash.com/photos/7_TSzqJms4w/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8NHx8ZGVjaGV0fGZyfDB8fHx8MTY5OTEzNDEwMHww&force=true&w=640"
+
+
+        pText.innerText = 'Contribuez à notre montagne de déchets non-recyclés en achetant nos produits 🤑';
+        divMessage.appendChild(pText);
+
+        imgDechet.style.margin = "auto"
+
+        divMessage.appendChild(imgDechet);
+
+        descriptionProduit.parentNode.insertBefore(divMessage, descriptionProduit.nextSibling);
+    }
+}
+
+function afterPrix(afterPricePanier){
     afterPricePanier.parentNode.insertBefore(warningMessageAmazon('57% de notre chiffre d’affaires en France est dissimulé dans des paradis fiscaux','Selon l\'association france.attac.org'), afterPricePanier.nextSibling);
 
     const amountPrice = document.getElementById("gutterCartViewForm")
@@ -188,7 +188,8 @@ if(afterPricePanier) {
         '<p>Avec ce geste, vous ne compenserez pas votre achat, ' +
         'mais c\'est mieux que rien du tout.</p>'), amountPrice.nextSibling);
 }
-else{
+
+function inProduct(){
     //pas de panier
     const titleProduct = document.getElementById("productTitle")
     if(titleProduct){//sur un article
@@ -259,6 +260,19 @@ else{
 
         buyNowButton.parentNode.insertBefore(fakeBuyNow, buyNowButton.nextSibling);
     }
+}
+
+showBanniere();
+// displayDescription();
+
+
+//Le saviez-vous panier
+const afterPricePanier = document.getElementById("deselect-all")
+if(afterPricePanier) {
+    afterPrix(afterPricePanier);
+}
+else{
+  inProduct();
 }
 
 
